4155847ca23e1c3813069d465c86bb5b977b7136
Guest NEK <kintnernoah@gmail.com>
2025-08-24 19:34:19 -0400

    Initial commit - add website files

---
+++ script.js
@@ -0,0 +1,5 @@
+// Handle form submission
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // stop page reload
+  alert("Thanks for contacting us! We'll get back to you soon.");
+});
